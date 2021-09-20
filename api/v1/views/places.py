f9e9618a1cb9c1ad2a64a3c2c32efe24fbef940a
coosoti <charlesosoti@gmail.com>
2021-09-20 13:23:18 +0300

    added a new view for Place objects that handles all default RESTFul API actions

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -23,7 +23,7 @@ def get_places(city_id):
 
 @app_views.route('/places/<string:place_id>', methods=['GET'],
                  strict_slashes=False)
-def get_place(city_id):
+def get_place(place_id):
     """retrieves one Place object with specific place_id"""
     place = storage.get("Place", place_id)
     if place is None:
